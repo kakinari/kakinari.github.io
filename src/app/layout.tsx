3c7dc48150966a113a8793968153012bae46037b
Takashi Kakinari <kakinari@me.com>
2026-06-12 19:33:09 +0900

    adjust layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
         <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       </head>
       <body
-        className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}
+        className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-100")}
       >
         <ThemeSwitcher />
         <div className="min-h-screen">{children}</div>
